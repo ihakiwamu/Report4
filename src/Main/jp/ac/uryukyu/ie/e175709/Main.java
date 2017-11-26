22b4a238139e33c3d233139f4c95e29843d99745
Kiwamu Iha <e175709@ie.u-ryukyu.ac.jp>
2017-11-26 21:44:16 +0900

    programming2_report4_2

--- src/Main/jp/ac/uryukyu/ie/e175709/Main.java
+++ src/Main/jp/ac/uryukyu/ie/e175709/Main.java
@@ -2,7 +2,15 @@ package jp.ac.uryukyu.ie.e175709;
 
 public class Main {
     public static void main(String[] args){
-        String str = null;
-        System.out.println(str.length());
+        try{
+            String str = null;
+            System.out.println(str.length());
+
+
+        }catch(NullPointerException e){
+            System.out.println("NullPointerExceptionが発生しました");
+            System.out.println(e.getMessage());
+
+        }
     }
 }
